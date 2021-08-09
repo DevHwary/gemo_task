da8d62f9a0afbd59f1b7890c127777986c28c0a6
Hwary <you@example.com>
2021-08-09 19:20:12 +0200

    add tests test_url_status

--- gemo_project/trending_languages/tests.py
+++ gemo_project/trending_languages/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+import unittest
+from datetime import datetime, timedelta
+import requests
 
-# Create your tests here.
+
+class SimpleTest(unittest.TestCase):
+
+    def test_url_status(self):
+        today = datetime.now()
+        thirty_days_ago = (today - timedelta(days=30)).strftime("%Y-%m-%d")
+        url = "https://api.github.com/search/repositories?q=created:>{0}&sort=stars&order=desc&page=1&per_page=100".format(thirty_days_ago)
+        response = requests.get(url)
+        self.assertEqual(response.status_code, 200)
