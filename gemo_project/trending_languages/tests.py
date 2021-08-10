2b3baed068b34d7091a129ab13c82d0ca827593e
Hwary <you@example.com>
2021-08-10 20:35:30 +0200

    add test test_list_top_100_language_view

--- gemo_project/trending_languages/tests.py
+++ gemo_project/trending_languages/tests.py
@@ -1,7 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 import unittest
 from datetime import datetime, timedelta
 import requests
+from . views import list_top_100_language
+from django.test import Client
 
 
 class SimpleTest(unittest.TestCase):
@@ -11,4 +13,13 @@ class SimpleTest(unittest.TestCase):
         thirty_days_ago = (today - timedelta(days=30)).strftime("%Y-%m-%d")
         url = "https://api.github.com/search/repositories?q=created:>{0}&sort=stars&order=desc&page=1&per_page=100".format(thirty_days_ago)
         response = requests.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_list_top_100_language_view(self):
+        """
+        test the list_top_100_language view
+        """
+        c = Client()
+        response = c.get('/trending_languages/top_100', HTTP_ACCEPT='application/json')
         self.assertEqual(response.status_code, 200)
